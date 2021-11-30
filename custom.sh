bc56baaa772e83f393cbf51602cac3ed62fa9e45
Zack Gu <asiaon123@gmail.com>
2021-12-01 00:16:16 +0800

    Add k2p 1806

--- custom.sh
+++ custom.sh
@@ -5,11 +5,6 @@ sed -i "s/192.168.1.1/$2/" package/base-files/files/bin/config_generate
 
 # Modify hostname
 sed -i "s/ImmortalWrt/$1/g" package/base-files/files/bin/config_generate
+
 # Modify hostname in chinese
-sed -i "/uci commit system/i\uci set system.@system[0].hostname=$1" package/emortal/default-settings/files/zzz-default-settings-chinese
-
-#Modify Default Lang
-sed -i "s/uci set luci.main.lang=auto/uci set luci.main.lang=zh_cn/g" package/emortal/default-settings/files/zzz-default-settings
-
-# Modify Openwrt Banner
-sed -i "s/ImmortalWrt/$1/g" package/emortal/default-settings/files/openwrt_banner
+# sed -i "/uci commit system/i\uci set system.@system[0].hostname=$1" package/emortal/default-settings/files/zzz-default-settings-chinese
