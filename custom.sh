526e04d016224ca3ea5ddaadeb92288c2bc32c3c
Zack Gu <asiaon123@gmail.com>
2021-12-07 11:39:15 +0800

    Add k2p lede build

--- custom.sh
+++ custom.sh
@@ -5,6 +5,7 @@ sed -i "s/192.168.1.1/$2/" package/base-files/files/bin/config_generate
 
 # Modify hostname
 sed -i "s/ImmortalWrt/$1/g" package/base-files/files/bin/config_generate
+sed -i "s/OpenWrt/$1/g" package/base-files/files/bin/config_generate
 
 # Modify hostname in chinese
 # sed -i "/uci commit system/i\uci set system.@system[0].hostname=$1" package/emortal/default-settings/files/zzz-default-settings-chinese
