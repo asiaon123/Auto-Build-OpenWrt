7ad2791418a585566bd2076925f7e160fcdedd95
Zack Gu <asiaon123@gmail.com>
2023-04-02 22:50:40 +0800

    Change default version to 5.4

--- custom.sh
+++ custom.sh
@@ -16,5 +16,5 @@ sed -i "s/ssid=OpenWrt/ssid=WeiFei@2040/" package/kernel/mac80211/files/lib/wifi
 # Update 7621 CPU Lock To 1000Hz
 # cp $3/991-mt7621-improve_cpu_clock.patch target/linux/ramips/patches-5.4/
 
-# Change kernel version
-# sed -i "s/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/" target/linux/ramips/Makefile
+# Change kernel version back to 5.4
+sed -i "s/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.4/" target/linux/ramips/Makefile
