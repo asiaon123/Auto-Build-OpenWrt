6b880d9d2c3759b91b70277c600b89184d8f3be2
ZackGu <asiaon123@gmail.com>
2023-07-04 00:16:17 +0800

    Adjustment and fix

--- custom.sh
+++ custom.sh
@@ -11,7 +11,10 @@ sed -i "s/OpenWrt/$1/g" package/base-files/files/bin/config_generate
 # sed -i "/uci commit system/i\uci set system.@system[0].hostname=$1" package/emortal/default-settings/files/zzz-default-settings-chinese
 
 # Modify wifi
+sed -i "s/country=US/country=AU/" package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i "s/ssid=OpenWrt/ssid=WeiFei@2040/" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# sed -i "s/encryption=none/encryption=psk2/" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# sed -i "s/encryption=none/encryption=psk2/" package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # Update 7621 CPU Lock To 1000Hz
 # cp $3/991-mt7621-improve_cpu_clock.patch target/linux/ramips/patches-5.4/
