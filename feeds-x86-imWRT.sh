cee0705f8affbc449fbdcb6bbf7015667f2f9984
Enzo.Gu <asiaon123@gmail.com>
2026-01-15 23:37:47 +0800

    Add feeds

--- feeds-x86-imWRT.sh
+++ feeds-x86-imWRT.sh
@@ -1,4 +1,7 @@
 # Add a feed source
 sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
-sed -i '$a src-git wolplus https://github.com/asiaon123/luci-app-wolplus.git' feeds.conf.default
-sed -i '$a src-git ssrplus https://github.com/fw876/helloworld.git' feeds.conf.default
+sed -i '$a src-git passwall2packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
+sed -i '$a src-git bandix https://github.com/timsaya/luci-app-bandix' feeds.conf.default
+sed -i '$a src-git bandixpackages https://github.com/timsaya/openwrt-bandix' feeds.conf.default
+sed -i '$a src-git luci_app_xray https://github.com/yichya/luci-app-xray' feeds.conf.default
+sed -i '$a src-git customs https://github.com/asiaon123/custom-openwrt-packages' feeds.conf.default
