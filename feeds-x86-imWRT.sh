54ac728ce26838928f8902f518e1d229fd5bfc33
Enzo.Gu <asiaon123@gmail.com>
2026-02-23 20:39:19 +0800

    Fix

--- feeds-x86-imWRT.sh
+++ feeds-x86-imWRT.sh
@@ -3,5 +3,5 @@ sed -i '$a src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwal
 sed -i '$a src-git passwall2packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git' feeds.conf.default
 sed -i '$a src-git bandix https://github.com/timsaya/luci-app-bandix' feeds.conf.default
 sed -i '$a src-git bandixpackages https://github.com/timsaya/openwrt-bandix' feeds.conf.default
-sed -i '$a src-git luci_app_xray https://github.com/yichya/luci-app-xray' feeds.conf.default
+# sed -i '$a src-git luci_app_xray https://github.com/yichya/luci-app-xray' feeds.conf.default
 sed -i '$a src-git customs https://github.com/asiaon123/custom-openwrt-packages' feeds.conf.default
