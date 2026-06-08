a500d39de3110396274693f4a3c2cbf9f947e328
Enzo.Gu <asiaon123@gmail.com>
2026-06-08 13:15:57 +0800

    Revert

--- feeds-x86-imWRT.sh
+++ feeds-x86-imWRT.sh
@@ -5,4 +5,3 @@ sed -i '$a src-git bandix https://github.com/timsaya/luci-app-bandix' feeds.conf
 sed -i '$a src-git bandixpackages https://github.com/timsaya/openwrt-bandix' feeds.conf.default
 # sed -i '$a src-git luci_app_xray https://github.com/yichya/luci-app-xray' feeds.conf.default
 sed -i '$a src-git customs https://github.com/asiaon123/custom-openwrt-packages' feeds.conf.default
-sed -i '$a src-git material3 https://github.com/KawaiiHachimi/luci-theme-material3' feeds.conf.default
